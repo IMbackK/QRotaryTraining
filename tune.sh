f035d51e429fca9f83979d58cadcc993b5f7b8e7
uvos <devnull@uvos.xyz>
2024-07-20 22:54:11 +0200

    add gpl3 text

--- tune.sh
+++ tune.sh
@@ -11,20 +11,24 @@ source $VENV_DIR/bin/activate
 python $SCRIPTS/train_dyamic/train_dynamic.py \
 	--model_name_or_path "huggyllama/llama-7b" \
 	--dataset "tatsu-lab/alpaca" \
-	--data_from_hub \
+	--dataset_type "hub" \
+	--eval_dataset_size 200 \
+	--source_max_len 1024 \
 	--do_train \
+	--do_eval \
+	--eval_steps 100 \
 	--reshufle_steps 50 \
-	--per_device_train_batch_size 8 \
-	--per_device_eval_batch_size 8 \
-	--gradient_checkpointing False \
-	--gradient_accumulation_steps 1 \
-	--num_train_epochs 2 \
+	--per_device_train_batch_size 2 \
+	--per_device_eval_batch_size 1 \
+	--gradient_checkpointing True \
+	--gradient_accumulation_steps 4 \
+	--epochs 3 \
 	--logging_dir $BASE_DIR/log \
 	--logging_steps 5 \
 	--learning_rate 1e-6 \
-	--save_steps 1000 \
-	--output_dir $BASE_DIR/llama-7b \
+	--save_steps 500 \
+	--output_dir $BASE_DIR/llama-7b-quant \
 	--adam8bit \
-	--max_instant_params 2000\
-	--churn_percent 50\
+	--churn_percent 100\
+	--max_instant_params 3000 \
 	--quantize
